d7d28834f6969feff17767804b0e707617f964d0
Nihal Razak <nihalr@sonicinteractive.org>
2021-09-28 04:22:43 +0800

    fix issue

--- resources/views/frontend/auth/passwords/reset.blade.php
+++ resources/views/frontend/auth/passwords/reset.blade.php
@@ -27,7 +27,7 @@
                                 <label for="password" class="col-md-4 col-form-label text-md-right">@lang('Password')</label>
 
                                 <div class="col-md-6">
-                                    <input type="password" id="password" name="password" class="form-control" placeholder="{{ __('Password') }}" maxlength="100" required autocomplete="password" />
+                                    <input type="password" id="password" name="password" class="form-control" placeholder="{{ __('Password') }}" maxlength="100" required autocomplete="new-password" />
                                 </div>
                             </div><!--form-group-->
 
