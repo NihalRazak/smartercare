be41678d9b533f90daec89308f29209e442d7c5d
Nihal Razak <nihalr@sonicinteractive.org>
2021-10-07 09:49:23 +0800

    fix issue

--- app/Http/Controllers/Frontend/HomeController.php
+++ app/Http/Controllers/Frontend/HomeController.php
@@ -20,7 +20,7 @@ class HomeController
      */
     public function index()
     {
-        return view('frontend.index');
+        return view('frontend.pages.search');
     }
 
     public function get_zip_codes(Request $request)
