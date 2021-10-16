4eb2653a35a6aaf2fe10c45e35fdf04053b604f2
Nihal Razak <nihalr@sonicinteractive.org>
2021-10-16 09:31:08 +0800

    fix issue

--- resources/views/emails/census.blade.php
+++ resources/views/emails/census.blade.php
@@ -1,23 +1,35 @@
-<div style="margin-bottom: 50px; text-align: center;">
-    <img src="https://360smartersearch.com/img/360-Smarter-Search-Wordss-Vertical-copy-1024x304.png" style="width: 300px;" />
+<div style="max-width: 570px; font-size: 16px; line-height: 1.5; margin: 0 auto;">
+    <div style="margin-bottom: 50px; text-align: center;">
+        <img src="https://360smartersearch.com/img/360-Smarter-Search-Wordss-Vertical-copy-1024x304.png" style="width: 300px;" />
+    </div>
+    <p>Courtesy of {{$company}}'s Executive TEAM, you and your family have access to a number of healthcare resources through 360 Smarter Care. The first is Smarter Search. Smarter Search Users typically save 40% on outpatient care. Mack will explain how and why.</p>
+    <div style="margin: 50px 0;">
+        <a href="https://vimeo.com/601842310"><img src="https://360smartersearch.com/img/video1.png" style="width: 100%;" /></a>
+    </div>
+    <p>Using Smarter Search is Intuitive. Jarry is here to walk you through.</p>
+    <div style="margin: 50px 0;">
+        <a href="https://vimeo.com/592851769"><img src="https://360smartersearch.com/img/video2.png" style="width: 100%;" /></a>
+    </div>
+    <p>Download your 360 app and connect to your 360 Smarter Search account. You can also access your 360 Smarter Search account here <a href="https://360smartersearch.com/login">https://360smartersearch.com/login</a> </p>
+    <div style="margin: 50px 0; display: flex;">
+        <div style="width: 50%;">
+            <div style="width: 50%;">
+                <a href="https://360smartersearch.com/360smartersearch.apk"><img src="https://360smartersearch.com/img/android.png" style="width: 100%;" /></a>
+                <p>This is a direct download for Android users</p>
+            </div>
+        </div>
+        <div style="width: 50%;">
+            <div style="width: 50%; float: right;">
+                <a href="#"><img src="https://360smartersearch.com/img/ios.png" style="width: 100%;" /></a>
+                <p>iPhone users download the iOS app from the Apple store.</p>
+            </div>
+        </div>
+    </div>
+    <p style="margin-bottom: 50px;">
+        Your Smarter Search account is created using <<b>{{ $email }}</b>>. Your temporary password is <b>Welcome#1</b>. Please change the password after your first login. 
+    </p>
+    <p style="margin: 0;">Sincerely,</p>
+    <p style="margin: 0; font-family: 'Comic sans ms', cursive; font-size: 26px;">Mary</p>
+    <p style="margin: 0;">Customer Care</p>
+    <p style="margin: 0;">information@360smartersearch.com</p>
 </div>
-<p>Courtesy of {{$company}}'s Executive TEAM, you and your family have access to a number of healthcare resources through 360 Smarter Care. The first is Smarter Search. Smarter Search Users typically save 40% on outpatient care. Mack will explain how and why.</p>
-<div style="margin: 50px 0;">
-    <a href="https://vimeo.com/601842310"><img src="https://360smartersearch.com/img/video1.png" style="width: 100%;" /></a>
-</div>
-<p>Using Smarter Search is Intuitive. Jarry is here to walk you through.</p>
-<div style="margin: 50px 0;">
-    <a href="https://vimeo.com/592851769"><img src="https://360smartersearch.com/img/video2.png" style="width: 100%;" /></a>
-</div>
-<p>Download your 360 app and connect to your 360 Smarter Search account. You can also access your 360 Smarter Search account here <a href="https://360smartersearch.com/login">https://360smartersearch.com/login</a> </p>
-<div style="margin: 50 0; text-align: center;">
-    <a href="https://360smartersearch.com/360smartersearch.apk"><img src="https://360smartersearch.com/img/android.png" style="height: 200px; width: auto;" /></a>
-    <a href="#"><img src="https://360smartersearch.com/img/ios.png" style="height: 200px; width: auto;" /></a>
-</div>
-<p>
-    Your Smarter Search account is created using <<b>{{ $email }}</b>>. Your temporary password is <b>Welcome#1</b>. Please change the password after your first login. 
-</p>
-<p style="margin: 0;">Sincerely,</p>
-<p style="margin: 0;">Mary</p>
-<p style="margin: 0;">Customer Care</p>
-<p style="margin: 0;">information@360smartersearch.com</p>
