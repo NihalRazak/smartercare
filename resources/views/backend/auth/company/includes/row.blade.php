83ac31690ee2997686f99ed4b53accb050f188bd
Nihal Razak <nihalr@sonicinteractive.org>
2021-10-30 01:16:15 +0800

    fix issue

--- resources/views/backend/auth/company/includes/row.blade.php
+++ resources/views/backend/auth/company/includes/row.blade.php
@@ -1,9 +1,23 @@
+@php
+    $providers = [
+        'All_Providers' => 'All Providers',
+        'Aetna' => 'Aetna', 
+        'Blue Shield' => 'Blue Cross',
+        'Cigna' => 'Cigna',
+        'Cofinity' => 'Cofinity',
+        'Humana' => 'Humana',
+        'Mecosta' => 'Mecosta',
+        'PHCS' => 'PHCS',
+        'United' => 'United Healthcare',
+        'UMPC' => 'UMPC'
+    ];
+@endphp
 <x-livewire-tables::bs4.table.cell>
     {{ $row->name }}
 </x-livewire-tables::bs4.table.cell>
 
 <x-livewire-tables::bs4.table.cell>
-    {{ $row->default_provider }}
+    {{ $providers[$row->default_provider] }}
 </x-livewire-tables::bs4.table.cell>
 
 <x-livewire-tables::bs4.table.cell>
